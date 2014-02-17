5da47c1a1582ca05ffbfb004886ed2137271d063
mikedonald <mcdonald.346@osu.edu>
2014-02-17 14:59:06 -0500

    initial commit from local git

---
+++ test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Michael McDonald on 2/17/14.
+//  Copyright (c) 2014 Michael McDonald. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
